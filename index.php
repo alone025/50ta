0fa3ea965dad310b894f9f689ba6211b20029d45
Forestest <hosting0web258@gmail.com>
2024-11-17 17:50:09 +0500

    New code

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Project</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+  <?php include_once 'components/header/header.php'; ?>
+
+  <main class="container">
+    <!-- Контент -->
+     <h3>hello</h3>
+  </main>
+
+  <?php include_once 'components/footer/footer.php'; ?>
+  
+  <!-- Bootstrap JS -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <!-- Custom JS -->
+  <script src="assets/js/scripts.js"></script>
+</body>
+</html>
---
+++ pages/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Project</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+
+  <main class="container">
+    <!-- Контент -->
+     <h3>hello</h3>
+  </main>
+
+ 
+</body>
+</html>
